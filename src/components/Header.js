aefed2a47fff56b7020490fc8a567e4a4dfe3d56
Barbara <barbaradasilvasantos23@gmail.com>
2023-10-05 19:52:31 -0300

    Aula de react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
+import { goToHome,goToProfile } from "../Routes/Coordinator";
 import {StyledHeader, StyledButton} from "../style"
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
 
+  const navigate = useNavigate()
   return (
     <StyledHeader>
-      <StyledButton >
+      <StyledButton onClick={()=>{goToHome(navigate)}} >
         Ir para página inicial
       </StyledButton>
-      <StyledButton >
+      <StyledButton onClick={()=>{goToProfile(navigate,"Barbara")}}>
         Ir para página de perfil
       </StyledButton>
     </StyledHeader>
